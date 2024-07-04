c685687c20844eaf62bff7101cc5797dc218394c
flowmaster1 <flowmaster195@gmail.com>
2024-07-04 19:19:13 +0700

    little fix

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -3,14 +3,15 @@ import mediapipe as mp
 import time
 
 class handDetector():
-    def __init__(self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
+    def __init__(self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5, modelComplexity = 1):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.trackCon = trackCon
+        self.modelComplex = modelComplexity
         
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, img , draw = True):
@@ -34,7 +35,7 @@ class handDetector():
 
 
 def main():
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(-1)
 
     pTime = 0
     cTime = 0
